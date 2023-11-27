4fb02a1fffb213acac3774bd6e8a84b4eb90ff43
saadghazal <saad.m.ghazal@icloud.com>
2023-11-27 16:24:45 +0300

    get the data from input file and print it on output file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /*
@@ -10,9 +12,19 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner inFile = new Scanner(new FileReader("in_file.txt")); // creating object of Scanner class to get data from a file
 
+        String firstName = inFile.next(); // getting the first name which is saad
+        String lastName = inFile.next(); // getting the last name which is Ghazal
+        int age = inFile.nextInt(); // getting the age which is 21
+        String userJob = inFile.nextLine(); // getting the job title of the user
+
+        PrintWriter outFile = new PrintWriter("out_file.txt"); // creating object of PrintWrite class which will hold the output text
+
+        outFile.print("User First Name: "+firstName+"\nUser Last Name: "+lastName+"\nUser Age: "+age+ "\nUser Job Title"+ userJob.toUpperCase()); // print on the output file
 
 
 
+
+        outFile.close();// you should close the outFile after using it to save the changes on it
         inFile.close(); // release the input file
     }
 }
