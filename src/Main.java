b45e0e12c989bf1ad285621aa8798d96183b76b3
saadghazal <saad.m.ghazal@icloud.com>
2023-11-27 16:17:33 +0300

    add project idea comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+/*
+ in this project we will get the user information from the input file
+ and store the information in output file
+ */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner inFile = new Scanner(new FileReader("in_file.txt")); // creating object of Scanner class to get data from a file
