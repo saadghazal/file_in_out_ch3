a6aa183e8dfdfabb1b16029dfe86b451f9029079
saadghazal <saad.m.ghazal@icloud.com>
2023-11-27 16:16:05 +0300

    creating text input file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner inFile = new Scanner(new FileReader("in_file.txt")); // creating object of Scanner class to get data from a file
 
+
+
+
+        inFile.close(); // release the input file
     }
 }
